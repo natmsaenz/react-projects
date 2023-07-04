dab10a74cfc580dba74d90c0f7eefa13140911a8
Natalia Madrigal Saenz <natalia.madrigal2941@alumnos.udg.mx>
2023-07-04 14:23:17 -0600

    added elements to the header component

---
+++ checklist/src/components/checklist/ChecklistHeader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ChecklistHeader(){
+    return(
+        <div className="header">
+            <h1><i class="fa-duotone fa-clipboard-list-check"></i>To-Do List</h1>
+        </div>  
+    )
+}
