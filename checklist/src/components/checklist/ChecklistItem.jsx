48190bfb19a6d5f5b773728334555095a4c7a433
Natalia Madrigal Saenz <natalia.madrigal2941@alumnos.udg.mx>
2023-07-05 15:33:48 -0600

    finished the ChecklistItem component

--- checklist/src/components/checklist/ChecklistItem.jsx
+++ checklist/src/components/checklist/ChecklistItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
+import PropTypes, { string } from "prop-types";
 
 function ChecklistItem({ item, onToggle, onDelete }) {
   const handleToggle = () => {
@@ -7,13 +7,23 @@ function ChecklistItem({ item, onToggle, onDelete }) {
   };
   const handleDelete = () => {
     onDelete(item.id);
-    };
-  
-    return(
-        <div>
-            <input type="checkbox" checked={item.completed} onChange={handleToggle} />
-        </div>
-    )
-  
+  };
+
+  return (
+    <div>
+      <input type="checkbox" checked={item.completed} onChange={handleToggle} />
+      <span className={item.completed ? "completed" : ""}>{item.text}</span>
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
 }
+ChecklistItem.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
+  }).isRequired,
+  onToggle: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
 export default ChecklistItem;
