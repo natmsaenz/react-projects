9af91d98adcc48af78589438a0b172035a69a139
Natalia Madrigal Saenz <natalia.madrigal2941@alumnos.udg.mx>
2023-07-13 22:42:46 -0600

    added styles to the header and animations to the icons

--- checklist/src/components/checklistHeader/ChecklistHeader.jsx
+++ checklist/src/components/checklistHeader/ChecklistHeader.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { faClipboard } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import "./ChecklistHeader.css"
 
 function ChecklistHeader(){
     return(
         <div className="header">
-            <h1> <FontAwesomeIcon icon={faClipboard} />To-Do List</h1>
+            <h1> <FontAwesomeIcon icon={faClipboard} className="icon"/>To-Do List</h1>
         </div>  
     )
 }
