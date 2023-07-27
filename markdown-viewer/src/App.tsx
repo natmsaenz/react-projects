cb987744ff7b3a5c95be12bdc6f279ec73de5d4a
Natalia Madrigal Saenz <natalia.madrigal2941@alumnos.udg.mx>
2023-07-27 16:35:01 -0600

    rendered the MarkdownEditor and MarkdownDisplay components in App.tsx

--- markdown-viewer/src/App.tsx
+++ markdown-viewer/src/App.tsx
@@ -1,11 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import './App.css'
+import MarkdownEditor from './markdownEditor/MarkdownEditor'
+import MarkdownDisplay from './markdownDisplay/MarkdownDisplay'
+import React, {useState} from "react";
+
 
 function App() {
+  const [markdownContent, setMarkdownContent] = useState<string>('');
 
   return (
     <>
       <div className='markdownBody'>
-        
+
+        <MarkdownEditor />
+        <MarkdownDisplay content={markdownContent} />
       </div>
      
     </>
