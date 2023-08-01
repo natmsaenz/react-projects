d64771421bc670d49850ee6b8ba41ecb929be6d5
Natalia Madrigal Saenz <natalia.madrigal2941@alumnos.udg.mx>
2023-08-01 15:29:55 -0600

    added new functions to toggle the sidebar

--- markdown-viewer/src/sidebar/Sidebar.tsx
+++ markdown-viewer/src/sidebar/Sidebar.tsx
@@ -1,29 +1,44 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faLightbulb, faFolderOpen} from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faLightbulb,
+  faFolderOpen,
+} from "@fortawesome/free-solid-svg-icons";
 
+const Sidebar = () => {
+  const cheatSheetUrl = "https://www.markdownguide.org/cheat-sheet/";
+  const repositoryUrl =
+    "https://github.com/natmsaenz/react-projects/tree/8354040c71bbccdb4e6ad4b9e10bb1e3ac303971/markdown-viewer";
 
-const Sidebar = () =>{
-    const cheatSheetUrl = "https://www.markdownguide.org/cheat-sheet/"
-    const repositoryUrl = "https://github.com/natmsaenz/react-projects/tree/8354040c71bbccdb4e6ad4b9e10bb1e3ac303971/markdown-viewer"
-
-return(
-    <div className="sidebar">
-        <div className="sidebarHeader">
-        <FontAwesomeIcon icon={faBars} /><h2>Markdown Editor</h2>
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const handleSidebarToggle = () => {
+    setIsSidebarOpen((prevIsOpen) => !prevIsOpen);
+  };
+  return (
+    <div className={`sidebar ${isSidebarOpen ? "open" : "closed"}`}>
+      <div className="sidebarHeader">
+        <div className="toggleButton" onClick={handleSidebarToggle}>
+        
+          <FontAwesomeIcon icon={faBars} />
         </div>
-        <ul className="sidebarLinks">
-            <li>
-                <a href={cheatSheetUrl} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLightbulb} /> Cheat Sheet</a>
-            </li>
-           <li>
-           <a href={repositoryUrl} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFolderOpen} /> Github repository</a>
-            
-           </li>
-        </ul>
+        <h2>Markdown Editor</h2>
+      </div>
+      <ul className="sidebarLinks">
+        <li>
+          <a href={cheatSheetUrl} target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLightbulb} /> Cheat Sheet
+          </a>
+        </li>
+        <li>
+          <a href={repositoryUrl} target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faFolderOpen} /> Github repository
+          </a>
+        </li>
+      </ul>
     </div>
-)
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
