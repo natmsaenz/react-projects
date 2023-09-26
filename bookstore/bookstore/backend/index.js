a5de19b50066192e495e853ad6a9f1ebbd6c5c06
Natalia Madrigal Saenz <natalia.madrigal2941@alumnos.udg.mx>
2023-09-25 20:14:29 -0600

    added some database connections, also, started creating the database with mongoDB

--- bookstore/bookstore/backend/index.js
+++ bookstore/bookstore/backend/index.js
@@ -1,1 +1,21 @@
-import { Express } from "express";
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+
+const app = express();
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("MERN tutorial");
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database")
+    app.listen(PORT, () => {
+      console.log(`App is listening to port:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
