17b94f68044d0c39bef37a52d8d0ac9d462193c6
Natalia Madrigal Saenz <natalia.madrigal2941@alumnos.udg.mx>
2023-09-25 20:48:57 -0600

    created a folder for the models and fixed issues with express module

--- bookstore/bookstore/backend/config.js
+++ bookstore/bookstore/backend/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://bookstore:GKizeH154PpPlnEA@cluster0.dlezskw.mongodb.net/books-collection?retryWrites=true&w=majority&appName=AtlasApp'
